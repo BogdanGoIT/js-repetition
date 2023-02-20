efeae11ee58dc99f08d175476c932e5779a76aaf
BogdanGoIT <92575228+BogdanGoIT@users.noreply.github.com>
2023-02-21 01:00:36 +0200

    delete js

--- js/script-1.js
+++
@@ -1,7 +0,0 @@
-const a = 'Переменная "а" в script-1.js';
-
-console.log(a);
-
-const b = 'Переменная "b" в script-1.js';
-
-console.log(b);
--- js/script-2.js
+++
@@ -1,7 +0,0 @@
-const a = 'Переменная "а" в script-2.js';
-
-console.log(a);
-
-const c = 'Переменная "c" в script-2.js';
-
-console.log(c);
